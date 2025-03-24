bee2953a0ae057002ffafc0ec31c9e835bb976e1
yooLucq <lucacoppolecchia07@gmail.com>
2025-03-24 18:02:27 +0100

    rep finished
    I have finished and completed css, html and js

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,12 @@
-let url = "https://randomuser.me/api/"
+// <>  `` $
 
-
+fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(data => {
+        let user = data.results[0];
+        document.querySelector('#photo').src = user.picture.large;
+        document.querySelector('#name').textContent = `${user.name.first} ${user.name.last}`;
+        document.querySelector('#gender').textContent = `Sesso: ${user.gender}`;
+        document.querySelector('#email').textContent = `Email: ${user.email}`;
+        document.querySelector('#address').textContent = `Indirizzo: ${user.location.city} ${user.location.country}`;
+    })
